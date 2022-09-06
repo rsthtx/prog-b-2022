7965924cd24b98f131ee685c411e375d6740bdcc
Rasmus Stougaard <rst@eucnordvest.dk>
2022-09-06 09:21:17 +0200

    Add python demos

---
+++ python-demo/function-demo.py
@@ -0,0 +1,11 @@
+
+# Defines a function
+def add(a,b):
+  return a + b
+  
+  
+print("starting")
+# calls the function
+c = add(3, 6)
+print("result: " + str(c))
+print("done")
---
+++ python-demo/while-demo.py
@@ -0,0 +1,5 @@
+spam = 0
+while spam < 10:
+  if spam % 2 == 0: 
+    print('Hello, world. '+ str(spam))
+  spam = spam + 1
