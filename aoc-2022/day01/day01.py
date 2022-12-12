b282d55a6fa5831d4d8cbe5315c0f939f18657bf
Rasmus Stougaard <rst@eucnordvest.dk>
2022-12-12 14:18:14 +0100

    Solve AoC 2022 day 1 a

---
+++ aoc-2022/day01/day01.py
@@ -0,0 +1,43 @@
+def solutionA(lines):
+  max_calories = 0
+  
+  calories = 0
+  for line in lines:
+    if not line:
+      calories = 0
+    else:
+      calories += int(line)
+      
+      if calories > max_calories:
+        max_calories = calories
+  
+  return max_calories
+
+
+def solutionB(lines):
+  # TODO: replace with code solving the problem
+  return -2 # Dummy result, deliberately wrong
+
+
+# Helper function for loading the problem data
+def load_data(fileName):
+  with open(fileName, "r") as input_data:
+    lines = input_data.readlines()
+  for i in range(len(lines)):
+    lines[i] = lines[i].strip()
+  return lines
+
+
+if __name__ == "__main__":
+  input_file_name = "dummy-input.txt"
+  # TODO: Uncomment line below to use real input
+  input_file_name = "input.txt"
+  
+  print("Loading data")
+  lines = load_data(input_file_name)
+  
+  resultA = solutionA(lines)
+  print(f"Solution for part A: {resultA}")
+
+  resultB = solutionB(lines)
+  print(f"Solution for part B: {resultB}")
