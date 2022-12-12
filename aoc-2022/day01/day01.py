503ae7e8bfa1f4eef98398f12a68ba8d3b1097e7
Rasmus Stougaard <rst@eucnordvest.dk>
2022-12-12 15:35:50 +0100

    Solve AoC 2022 day 1 b

--- aoc-2022/day01/day01.py
+++ aoc-2022/day01/day01.py
@@ -13,10 +13,27 @@ def solutionA(lines):
   
   return max_calories
 
-
 def solutionB(lines):
-  # TODO: replace with code solving the problem
-  return -2 # Dummy result, deliberately wrong
+  top_list = [0,0,0]
+  calories = 0
+  for line in lines:
+    if line == "":
+      update_top_list(calories, top_list)
+      calories = 0
+    else:
+      calories += int(line)
+  
+  # make sure the last elf is included
+  update_top_list(calories, top_list)
+    
+  return sum(top_list)
+
+def update_top_list(calories, top_list):
+  for i, cal in enumerate(top_list):
+    if calories > cal:
+      top_list.insert(i, calories)
+      top_list.pop()
+      break
 
 
 # Helper function for loading the problem data
@@ -31,7 +48,7 @@ def load_data(fileName):
 if __name__ == "__main__":
   input_file_name = "dummy-input.txt"
   # TODO: Uncomment line below to use real input
-  input_file_name = "input.txt"
+  # input_file_name = "input.txt"
   
   print("Loading data")
   lines = load_data(input_file_name)
