2979d60a210da8de71fba54817fc1f666368a077
Rasmus Stougaard <rasmus.stougaard@gmail.com>
2023-10-03 09:40:15 +0200

    python demos

---
+++ python-demo/list-iteration-demo.py
@@ -0,0 +1,11 @@
+my_list = [6,7,8]
+
+for value in my_list:
+  print(value)
+  
+for idx in range(len(my_list)):
+  value = my_list[idx]
+  print(idx, ":", value)
+
+for idx, value in enumerate(my_list):
+  print(idx, ":", value)
